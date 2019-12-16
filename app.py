978a91c308d1d511154317f118d46084a7a32e71
Matt-F90 <matthew.fairweather@ymail.com>
2019-12-16 12:45:29 +0000

    github push

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe'
+app.config["MONGO_URI"] = 'mongodb+srv://matt_f:Mattmatt90@myfirstcluster-y3quy.mongodb.net/recipes?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", meals=mongo.db.meals.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
